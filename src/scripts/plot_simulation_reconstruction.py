952295764bdd58c241c468dd5a2f1d618915708d
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-07-19 16:24:54 +0800

    Update plot_simulation_reconstruction.py

--- src/scripts/plot_simulation_reconstruction.py
+++ src/scripts/plot_simulation_reconstruction.py
@@ -2,6 +2,7 @@ from figaro.load import load_density
 from figaro.plot import plot_median_cr
 from figaro import plot_settings
 import numpy as np
+from matplotlib import pyplot as plt
 import paths
 
 label = 'simulation'
@@ -10,12 +11,22 @@ outdir = paths.data / 'simulation'
 draws = load_density(outdir / f'draws/draws_observed_{label}.json')
 obs_samples = np.loadtxt(outdir / 'obs_samples.txt')
 
-fig = plot_median_cr(draws, hierarchical=True, save=True, show=False)
-ax = fig.axes[0]
-ax.hist(obs_samples, bins = int(np.sqrt(len(obs_samples))), histtype = 'step', density = True, label = '$\mathrm{median}(Y_t)$', color = 'tab:red')
-ax.set_xlim(0,200)
-ax.set_ylim(0)
-ax.set_xlabel('$m^z_1\ [\mathrm{M}_\odot]$')
-ax.set_ylabel('$\mathrm{Density}$')
-ax.legend()
+fig, (ax1, ax2) = plt.subplots(2, figsize=(7,6), gridspec_kw={'hspace': 0.3})
+fig = plot_median_cr(draws, hierarchical=True, save=True, show=False, fig=fig)
+ax1.hist(obs_samples, bins = int(np.sqrt(len(obs_samples))), histtype = 'step', density = True, label = '$\mathrm{median}(Y_t)$', color = 'tab:red')
+ax1.set_xlim(0,200)
+ax1.set_ylim(0)
+ax1.set_xlabel('$m^z_1\ [\mathrm{M}_\odot]$')
+ax1.set_ylabel('$\mathrm{Density}$')
+ax1.legend()
+m_z = np.linspace(0,200,1000)
+ax2.plot(m_z, draws[0].pdf(m_z), alpha = 0.5, color = 'tab:blue', label = '$\mathrm{(H)DPGMM}$')
+for i, draw in enumerate(draws[1:10]):
+    ax2.plot(m_z, draw.pdf(m_z), alpha = 0.5, color = 'tab:blue')
+ax2.hist(obs_samples, bins = int(np.sqrt(len(obs_samples))), histtype = 'step', density = True, label = '$\mathrm{median}(Y_t)$', color = 'tab:red')
+ax2.set_xlim(0,200)
+ax2.set_ylim(0)
+ax2.set_xlabel('$m^z_1\ [\mathrm{M}_\odot]$')
+ax2.set_ylabel('$\mathrm{Density}$')
+ax2.legend()
 fig.savefig(paths.figures / 'simulation_reconstruction.pdf', bbox_inches='tight')
