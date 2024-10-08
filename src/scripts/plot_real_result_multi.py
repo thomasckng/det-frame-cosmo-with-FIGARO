0763b1963bc25d85b8341c0fcabd1e67a88a35b5
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-10-08 09:57:38 +0800

    Update plot_real_result_multi.py

--- src/scripts/plot_real_result_multi.py
+++ src/scripts/plot_real_result_multi.py
@@ -6,7 +6,7 @@ import paths
 f = np.load(paths.data / 'real/multi' / '3_Powell.npz')
 result = f['result']
 result = result[(result[:, 0] > 30) & (result[:, 0] < 120)]
-result = result[(result[:, 2] > -5) & (result[:, 2] < 5)]
+result = result[result[:, 2] > 0]
 parameters = ["$H_0$", "$\\alpha$", "$\\kappa$"]
 
 fig = corner(result, labels=parameters, color='steelblue', truth_color='red', levels = [0.3935, 0.9], plot_datapoints=False, plot_density=False, smooth=1, fill_contours=True, contourf_kwargs={'colors': ['white', 'darkturquoise', 'mediumturquoise'], 'alpha': [1, 0.2, 0.5]}, contour_kwargs={'linewidths': 1})
