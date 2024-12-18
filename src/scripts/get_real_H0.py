0ceb3a04fcb34b4d94b4c40e1ef985b7767a08fe
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-12-18 23:15:32 +0800

    Use 1 sigma instead of 90%

--- src/scripts/get_real_H0.py
+++ src/scripts/get_real_H0.py
@@ -5,8 +5,8 @@ f = np.load(paths.data / 'real/multi' / '4c_Powell.npz')
 result = f['result']
 H0 = result[:, 0]
 med = np.median(H0)
-low = np.percentile(H0, 10)
-high = np.percentile(H0, 90)
+low = np.percentile(H0, 50-68/2)
+high = np.percentile(H0, 50+68/2)
 
 with open(paths.output/'real_H0.txt', 'w') as f:
     f.write(f'$H_0 = {med:.2f}^{{+{high - med:.2f}}}_{{-{med - low:.2f}}} \, \mathrm{{km/s/Mpc}}$')
