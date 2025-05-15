ed0966166ea3a1d13bb732633c69e3dbf2f639dd
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-05-15 11:42:50 +0800

    Refactor multi simulation plotting to iterate over parameter sets and handle exceptions; improve file loading and plotting logic.

--- src/scripts/plot_simulation_result_H0.py
+++ src/scripts/plot_simulation_result_H0.py
@@ -9,7 +9,7 @@ true_H0 = Planck18.h*100
 
 fig, ax = plt.subplots()
 ax.hist(H0_samples, bins = int(np.sqrt(len(H0_samples))), histtype = 'step', density = True, color = 'tab:blue')
-percs = np.percentile(H0_samples, [5, 16, 50, 84, 95])
+percs = np.percentile(H0_samples, [5, 15.87, 50, 84.13, 95])
 ax.axvline(true_H0, lw = 0.7, ls = '--', c = 'red', label = f'$H_0={true_H0:.1f}\ (\mathrm{{Simulated}})$')
 ax.axvline(percs[2], c = 'steelblue', lw=0.7, label = f'$H_0={percs[2]:.1f}^{{+{percs[3]-percs[2]:.1f}}}_{{-{percs[2]-percs[1]:.1f}}}$')
 ax.axvspan(percs[1], percs[3], alpha=0.35, color='mediumturquoise')
--- src/scripts/plot_simulation_result_multi.py
+++ src/scripts/plot_simulation_result_multi.py
@@ -4,10 +4,52 @@ from figaro.cosmology import Planck18
 from figaro import plot_settings
 import paths
 
-f = np.load(paths.data / 'simulation/multi' / '4b_Powell.npz')
-result = f['result']
-simulated_truth = {'$H_0$': Planck18.h*100, '$\\alpha$': 3.5, '$\\mu$': 35, '$\\sigma$': 5, '$\\delta$': 5, '$w$': 0.2, '$m_\mathrm{min}$': 5, '$m_\mathrm{max}$': 90}
-parameters = ["$H_0$", "$\\alpha$", "$\\mu$", "$\\sigma$"]
+param_dict = {
+    '2a': ['$H_0$', '$\\alpha$'],
+    '2b': ['$H_0$', '$\\mu$'],
+    '3a': ['$H_0$', '$\\alpha$', '$\\kappa$'],
+    '3b': ['$H_0$', '$\\mu$', '$\\kappa$'],
+    '4a': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\sigma$'],
+    '4b': ['$H_0$', '$\\mu$', '$\\sigma$', '$\\kappa$'],
+    '4c': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\kappa$'],
+    '5a': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\sigma$', '$w$'],
+    '5b': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\sigma$', '$\\kappa$'],
+    '6': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\sigma$', '$w$', '$\\kappa$'],
+    '9': ['$H_0$', '$\\alpha$', '$\\mu$', '$\\sigma$', '$w$', '$\\delta$', '$m_\mathrm{min}$', '$m_\mathrm{max}$', '$\\kappa$'],
+}
 
-fig = corner(result, labels=parameters, truths=[simulated_truth[param] for param in parameters], color='steelblue', truth_color='red', levels = [0.3935, 0.95], plot_datapoints=False, plot_density=False, smooth=1, fill_contours=True, contourf_kwargs={'colors': ['white', 'darkturquoise', 'mediumturquoise'], 'alpha': [1, 0.2, 0.5]}, contour_kwargs={'linewidths': 1}, show_titles=True)
-fig.savefig(paths.figures / "simulation_result_multi.pdf", bbox_inches='tight')
+for key, parameters in param_dict.items():
+    try:
+        f = np.load(paths.data / 'simulation/multi' / f'{key}_Powell.npz')
+        result = f['result']
+        simulated_truth = {'$H_0$': Planck18.h*100, '$\\alpha$': 3.5, '$\\mu$': 35, '$\\sigma$': 5, '$\\delta$': 5, '$w$': 0.2, '$m_\mathrm{min}$': 5, '$m_\mathrm{max}$': 90, '$\\kappa$': 0}
+
+        fig = corner(result,
+                    labels=parameters,
+                    range= [0.8]*len(parameters),
+                    truths=[simulated_truth[param] for param in parameters],
+                    color='steelblue',
+                    truth_color='red',
+                    levels = [0.3935,
+                            0.8647,
+                            #    0.9,
+                            ],
+                    plot_datapoints=False,
+                    plot_density=False,
+                    smooth=1,
+                    fill_contours=True,
+                    contourf_kwargs={'colors': ['white', 'darkturquoise', 'mediumturquoise'], 'alpha': [1, 0.2, 0.5]},
+                    contour_kwargs={'linewidths': 1}, show_titles=True)
+        fig.savefig(paths.figures / f'simulation_result_{key}.pdf', bbox_inches='tight')
+        fig.clf()
+        f.close()
+        print(f"Plot saved for key: {key}.")
+    except FileNotFoundError:
+        print(f"File not found for key: {key}. Skipping...")
+    except Exception as e:
+        print(f"An error occurred for key: {key}. Error: {e}")
+        try:
+            f.close()
+        except:
+            pass
+    print()
